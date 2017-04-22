6785a534eee4f9e5fa23ac771b6369af5e5ca775
Mr.Stalin <fakels@yandex.ru>
2017-04-22 22:02:22 +0300

    Init Commit

---
+++ HeadPatcher/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace HeadsPatcher
+{
+    static class Program
+    {
+        /// <summary>
+        /// Main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new HeadsFrmPatcher());
+        }
+    }
+}
